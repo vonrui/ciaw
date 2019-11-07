6fe582bbb49495cfae647f0b1b33e24255b4bc2c
vonrui <45139721+vonrui@users.noreply.github.com>
2019-11-07 22:09:23 +0800

    Update vecrot.m
    correct form in the second term should be 
    cross（axis，vector）*sin（angle），i hope i am right

--- CIAW SAB - Todas as simulações/sab/vecrot.m
+++ CIAW SAB - Todas as simulações/sab/vecrot.m
@@ -1,9 +1,9 @@
-% Fórmula de Rotação de Rodrigues
+% FÃ³rmula de RotaÃ§Ã£o de Rodrigues
 
 function new_vector = vecrot(vector, axis, angle)
     axis = vecdir(axis);
     
     new_vector = vector * cos(angle) + ...
-                 cross(vector, axis) * sin(angle) + ...
+                 cross(axisï¼Œvector) * sin(angle) + ...
                  axis*dot(axis, vector)*(1 - cos(angle));
 end
